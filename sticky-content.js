9e97ca8a86105099cb4abb35322877a05395f19b
Vladyslav <grishin.vlad23@gmail.com>
2019-08-28 18:18:42 +0300

    define object

--- sticky-content.js
+++ sticky-content.js
@@ -1,6 +1,21 @@
 const stickyContent = Object.create(null);
 
-Object.defineProperties(stickyContent, {});
+Object.defineProperties(stickyContent, {
+    'selector': {
+        value: '.sticky_content'
+    },
+    'nodes': {
+        writable: true
+    }
+});
+
+stickyContent.sticky = function () {
+
+};
+
+stickyContent.unstick = function () {
+
+};
 
 stickyContent.init = function () {
 
